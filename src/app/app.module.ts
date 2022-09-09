790527aa2e4859fdf876b4966c9a695691e9b224
minhdat2708 <minhdat01.dev@gmail.com>
2022-09-09 18:11:17 +0700

    button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import {DashboardModule} from "./component/dashboard/dashboard.module";
+import {RouterModule} from "@angular/router";
+import { ButtonComponent } from './component/button/button.component';
+import {ButtonModule} from "./component/button/button.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    DashboardModule,
+    ButtonModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/button/button.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ButtonRoutingModule } from './button-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ButtonRoutingModule
+  ]
+})
+export class ButtonModule { }
