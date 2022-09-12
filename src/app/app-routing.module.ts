c0f94ad5d08e64540e01cb203700cf076372e61f
minhdat2708 <minhdat01.dev@gmail.com>
2022-09-12 14:45:19 +0700

    complete route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,103 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./component/dashboard/dashboard.component";
+import {ButtonComponent} from "./component/button/button.component";
+import {CardComponent} from "./component/card/card.component";
+import {ColorsComponent} from "./component/colors/colors.component";
+import {BordersComponent} from "./component/borders/borders.component";
+import {AnimationComponent} from "./component/animation/animation.component";
+import {OtherComponent} from "./component/other/other.component";
+import {TablesComponent} from "./component/tables/tables.component";
+import {ChartsComponent} from "./component/charts/charts.component";
+import {ErrorPageComponent} from "./component/error-page/error-page.component";
+import {BlankPageComponent} from "./component/blank-page/blank-page.component";
+import {LoginComponent} from "./component/login/login.component";
+import {RegisterComponent} from "./component/register/register.component";
+import {ForgotPasswordComponent} from "./component/forgot-password/forgot-password.component";
+import {AdminComponent} from "./pages/admin/admin.component";
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'admin/dashboard',
     pathMatch: 'full'
   },
   {
-    path: 'dashboard',
-    loadChildren: () =>
-      import('./component/dashboard/dashboard-routing.module').then(
-        m => m.DashboardRoutingModule
-      ),
-  },{
-    path: 'button',
-    loadChildren: () =>
-      import('./component/button/button-routing.module').then(
-        m => m.ButtonRoutingModule
-      ),
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      {path: 'dashboard', component: DashboardComponent, title: 'sb-admin-2 Dashboard'},
+      {
+        path: 'component/button',
+        component: ButtonComponent,
+        title: 'Buttons'
+      },
+      {
+        path: 'component/card',
+        component: CardComponent,
+        title: 'Cards'
+      },
+      {
+        path: 'utilities/colors',
+        component: ColorsComponent,
+        title: 'Colors'
+      },
+      {
+        path: 'utilities/borders',
+        component: BordersComponent,
+        title: 'Borders'
+      },
+      {
+        path: 'utilities/animations',
+        component: AnimationComponent,
+        title: 'Animations'
+      },
+      {
+        path: 'utilities/others',
+        component: OtherComponent,
+        title: 'Others'
+      },
+      {
+        path: 'charts',
+        component: ChartsComponent,
+        title: 'Charts'
+      },
+      {
+        path: 'tables',
+        component: TablesComponent,
+        title: 'Tables'
+      },
+      {
+        path: 'pages/404-page',
+        component: ErrorPageComponent,
+        title: 'Error Pages'
+      },
+      {
+        path: 'pages/blank-page',
+        component: BlankPageComponent,
+        title: 'Blank Pages'
+      },
+    ]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register'
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+    title: 'Forgot Password'
+  },
+  {
+    path: '**',
+    redirectTo: 'pages/404-page',
+    pathMatch: 'full',
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
-import {DashboardModule} from "./component/dashboard/dashboard.module";
 import {RouterModule} from "@angular/router";
 import { ButtonComponent } from './component/button/button.component';
-import {ButtonModule} from "./component/button/button.module";
+import { CardComponent } from './component/card/card.component';
+import { ColorsComponent } from './component/colors/colors.component';
+import { BordersComponent } from './component/borders/borders.component';
+import { AnimationComponent } from './component/animation/animation.component';
+import { OtherComponent } from './component/other/other.component';
+import { TablesComponent } from './component/tables/tables.component';
+import { ChartsComponent } from './component/charts/charts.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
+import { ErrorPageComponent } from './component/error-page/error-page.component';
+import { BlankPageComponent } from './component/blank-page/blank-page.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import {NgxPaginationModule} from "ngx-pagination";
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    ButtonComponent
+    ButtonComponent,
+    CardComponent,
+    ColorsComponent,
+    BordersComponent,
+    AnimationComponent,
+    OtherComponent,
+    TablesComponent,
+    ChartsComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    ErrorPageComponent,
+    BlankPageComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    DashboardModule,
-    ButtonModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
