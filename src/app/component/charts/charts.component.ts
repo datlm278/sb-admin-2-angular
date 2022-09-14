33026b1ece1b7623f3ef79f51be23d17ec945e73
minhdat2708 <minhdat01.dev@gmail.com>
2022-09-14 11:55:05 +0700

    complete route

--- src/app/component/charts/charts.component.ts
+++ src/app/component/charts/charts.component.ts
@@ -23,8 +23,75 @@ export class ChartsComponent implements OnInit {
   };
 
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
-    responsive: true,
+    scales: {
+      x: {
+        time: {
+          unit: "month"
+        },
+        grid: {
+          display: true,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 12,
+        },
+      },
+      y: {
+        ticks: {
+          maxTicksLimit: 5,
+        },
+        grid: {
+          color: "rgb(234, 236, 244)",
+          borderColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          lineWidth: [2]
+        },
+        min: 0
+      }
+    },
+    plugins: {
+      legend: {
+        display: false,
+      }
+    }
   };
+
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [{
+      label: "Earnings",
+      backgroundColor: "rgba(78, 115, 223, 0.05)",
+      borderColor: "rgba(78, 115, 223, 1)",
+      pointRadius: 3,
+      pointBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointBorderColor: "rgba(78, 115, 223, 1)",
+      pointHoverRadius: 3,
+      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+      pointHitRadius: 10,
+      pointBorderWidth: 2,
+      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      fill: "start",
+    }]
+  };
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    elements: {
+      line: {
+        tension: 0.3
+      }
+    },
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    }
+  };
+  public lineChartLegend = true;
 
   constructor() { }
 
